c6d166ac88be904fcd0945068f8d28670e600f09
speiser <sebastian.speiser@hft-stuttgart.de>
2020-12-15 14:51:10 +0100

    Moving downloads to jest config instead of GH actions

--- scripts/downloadTestFiles.ts
+++ scripts/downloadTestFiles.ts
@@ -14,43 +14,37 @@ const testFiles =
                 'https://file-examples-com.github.io/uploads/2017/02/zip_10MB.zip',
                 'https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4']);
 
-const promises: Promise<void>[] = [];
-if (!fs.existsSync('testdata/data/lock')) {
-    fs.mkdirSync('testdata/data', { recursive: true });
-}
 
-for (const testFile of testFiles) {
-    console.log('Downloading ' + testFile);
-    const filename = testFile.split('/').pop();
-    const path = 'testdata/data/' + filename;
-    if (!fs.existsSync(path)) {
-        promises.push(new Promise<void>((resolve) => {
-            const file = fs.createWriteStream(path);
-            // const fd = fs.openSync(path, 'w');
-            https.get(testFile, (response) => {
-                response.pipe(file);
-                // response.on('data', (data) => {
-                //     // file.write(data);
-                //     fs.writeSync(fd, data);
-                // });
-                // response.on('close', () => {
-                //     //file.close();
-                //     fs.closeSync(fd);
-                // }).on('error', (error) => {
-                //     console.log(`Download failed for ${testFile}: ${error}`);
-                //     throw new Error(`${error}`);
-                // });
-            }).on('end', () => {
-                file.close();
-                resolve();
-            }).on('error', (error) => {
-                console.log(`Download failed for ${testFile}: ${error}`);
-                resolve();
-            })
-        }));
+export default async function downloadTestFiles(): Promise<void> {
+    const promises: Promise<void>[] = [];
+    if (!fs.existsSync('testdata/data/lock')) {
+        fs.mkdirSync('testdata/data', { recursive: true });
     }
-}
 
-process.on('beforeExit', async () => {
-    await Promise.all(promises);
-});
+    for (const testFile of testFiles) {
+        console.log('Downloading ' + testFile);
+        const filename = testFile.split('/').pop();
+        const path = 'testdata/data/' + filename;
+        if (!fs.existsSync(path)) {
+            promises.push(new Promise<void>((resolve) => {
+                const file = fs.createWriteStream(path);
+                https.get(testFile, (response) => {
+                    response.pipe(file);
+                }).on('close', () => {
+                    console.log('Download finished: ' + testFile);
+                    file.close();
+                    resolve();
+                }).on('error', (error) => {
+                    console.log(`Download failed for ${testFile}: ${error}`);
+                    resolve();
+                })
+            }));
+        }
+    }
+    console.log('Downloading ...');
+    await Promise.all(promises).then();
+    console.log('Downloading ... done');
+}
+// process.on('beforeExit', async () => {
+//     await Promise.all(promises);
+// });
